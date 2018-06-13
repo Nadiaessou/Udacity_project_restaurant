b0379cb7586e5918c97cff4740fb822a91068177
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2018-06-13 02:00:24 +0200

    Fix: mapbox and reponsive for home page
    Ajout de la carte mapbox et mise en place du responsive pour la home page

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibmFkaWFlc3NvdSIsImEiOiJjamlieW1vbTMwMDBtM3JtaGplZ3N0ZGp3In0.Shpv7nBXIdz59flqSIQXNA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
